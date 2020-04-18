b8d4f490851f869b7cfa99ee90d2c638cf604331
Matthias Huygelen <Matthias.Huygelen@realdolmen.com>
2020-04-19 01:44:49 +0200

    oefening events 2

---
+++ Oefening-Events-2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Oefening_Events_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef de countdown tijd in ( HH:mm:ss ) :");
+            string timeInput = Console.ReadLine();
+            var timeList = timeInput.Split(':').Select(x => Convert.ToInt32(x)).ToArray();
+            Timer timer = new Timer(timeList[0], timeList[1], timeList[2]);
+            timer.ItsTime += Timer_ItsTime;
+            timer.CurrentTime += Timer_CurrentTime;
+            timer.Start();
+        }
+
+        private static void Timer_CurrentTime(object sender, string e)
+        {
+            Console.Clear();
+            Console.CursorVisible = false;
+            Console.SetCursorPosition((Console.WindowWidth - e.Length) / 2, Console.WindowHeight /2);
+            Console.WriteLine(e);
+        }
+
+        private static void Timer_ItsTime(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.CursorVisible = false;
+                Console.BackgroundColor = Console.BackgroundColor == ConsoleColor.White ? ConsoleColor.Black : ConsoleColor.White;
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ Oefening-Events-2/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Oefening_Events_2
+{
+    public class Timer
+    {
+        public event EventHandler ItsTime;
+        public event EventHandler<string> CurrentTime;
+        public TimeSpan TimeSpan { get; set; }
+        public Timer(int houres , int minutes , int seconds)
+        {
+            TimeSpan = new TimeSpan(houres, minutes, seconds);
+        }
+        public void Start()
+        {
+            while (TimeSpan.TotalSeconds > 0)
+            {
+                TimeSpan = TimeSpan.Add(new TimeSpan(0, 0, -1));
+                CurrentTime.Invoke(this, TimeSpan.ToString());
+                Thread.Sleep(1000);
+            }
+            ItsTime.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
